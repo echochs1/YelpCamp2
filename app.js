4ab5a71e75bb5ffb9ad83c63e9dac9ff06e4c5af
cho-bani <echochs@bu.edu>
2022-12-28 18:51:35 -0500

    added a better secret

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ const store = MongoDBStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret: `${process.env.SECRET}`
     }
 });
 
@@ -112,7 +112,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret: `${process.env.SECRET}`,
     resave: false,
     saveUninitialized: true,
     cookie: {
