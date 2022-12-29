67ae41ec76a96e9388bb04f2d561c751dd8f01a3
cho-bani <echochs@bu.edu>
2022-12-28 19:00:57 -0500

    updated port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { urlencoded } = require('express');
 
 const userRoutes = require('./routes/users');
